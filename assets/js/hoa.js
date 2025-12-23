a047026a46661f45e6f8d7cd53b405e4b7631953
TuanHuyyReal <lytuanhuy1406@gmail.com>
2025-12-23 19:25:06 +0700

    Update hoa.js

--- assets/js/hoa.js
+++ assets/js/hoa.js
@@ -5,7 +5,7 @@ async function handleApiResponse(category, difficulty) {
     fetch(QUIZ_API_URL)
       .then((response) => response.json())
       .then((question_data) => {
-        const categories = ["Liên kết Ion"];
+        const categories = ["Sử dụng và quản lí tiền tiêu vặt"];
         const categoryIndex = parseInt(category) - 9; // Adjust index based on API categories
         const categoryContext = categories[categoryIndex];
         let score = 0; // Initialize score
